de9b1a41d95e8e2f4f1ee86c40a058a04ea6e74a
GianBaeza <gianb04@gmail.com>
2024-07-01 22:46:59 -0300

    nav

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,27 +1,38 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import "./NavBar.css";
-import logo from "../Navbar/img/gb.png";
+import React, { useState } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Link } from "react-scroll";
-import { useState, useEffect } from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setIsNavExpanded(false);
+  };
+
   return (
-    <Navbar expand="lg" className=" navbar-custom " fixed="top">
+    <Navbar
+      expand="lg"
+      className={`navbar-custom ${isNavExpanded ? 'open' : ''}`}
+      fixed="top"
+      expanded={isNavExpanded}
+    >
       <Container className="container-custom-nav">
         <Navbar.Toggle
-          aria-controls="basic-navbar-nav "
+          aria-controls="basic-navbar-nav"
           className="nav-toggler-custom"
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
         />
         <Navbar.Collapse id="basic-navbar-nav" className="nav-Collapse-custom">
           <Nav className="mx-auto">
             <Link
               className="nav-link-custom"
-              to="/"
+              to="home"
               spy={true}
               smooth={true}
-              offset={50}
+              offset={-70}
               duration={500}
+              onClick={handleNavClick}
             >
               Home
             </Link>
@@ -30,8 +41,9 @@ const NavBar = () => {
               to="about"
               spy={true}
               smooth={true}
-              offset={50}
+              offset={-70}
               duration={500}
+              onClick={handleNavClick}
             >
               About
             </Link>
@@ -40,8 +52,9 @@ const NavBar = () => {
               to="skills"
               spy={true}
               smooth={true}
-              offset={50}
+              offset={-70}
               duration={500}
+              onClick={handleNavClick}
             >
               Skills
             </Link>
@@ -50,12 +63,21 @@ const NavBar = () => {
               to="contact"
               spy={true}
               smooth={true}
-              offset={50}
+              offset={-70}
               duration={500}
+              onClick={handleNavClick}
             >
               Contact
             </Link>
           </Nav>
+          {/* Botón de cerrar */}
+          <Button
+            className="navbar-close-btn"
+            onClick={handleNavClick}
+            aria-label="Close"
+          >
+            &times;
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
