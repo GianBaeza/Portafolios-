9dec11b4074210bd36fd0c9470dea251f4cafb93
GianBaeza <gianb04@gmail.com>
2024-07-04 17:38:27 -0300

    animacion

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,10 +1,20 @@
 import "../skills/skills.css";
+import "animate.css";
+import { useInView } from "react-intersection-observer";
 
 function ListSkills({ name, img }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
   return (
     <div className="container">
-      <ul>
-        <li>
+      <ul
+        className={`list-item ${
+          inView ? "animate__animated animate__fadeInRight" : ""
+        }`}
+      >
+        <li ref={ref}>
           <p>{name}</p>
           <img src={img} alt="icon" />
         </li>
